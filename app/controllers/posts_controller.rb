97853abda173928b81cbb3709f95c6a00ebc9c67
YS <35660051+Yay0v0@users.noreply.github.com>
2018-01-23 18:02:33 +0900

    Create post page and update all pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,7 @@ class PostsController < ApplicationController
   def top
     @posts = ["テストです　テストです　テストです　テストです　テストです　テストです　テストです　テストです　テストです　", "テストです　テストです　テストです　テストです　テストです　テストです　テストです　テストです　テストです　テストです　テストです　テストです　"]
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'top' => 'posts#top'
+  get 'new' => 'posts#new'
 
   get '/' => 'home#login'
 
